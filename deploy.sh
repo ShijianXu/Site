9ed9401c850954793331153dccf72f5d39e47ffb
shijianxu <13260906215@163.com>
2022-02-09 12:49:19 +0100

    return back to old hugo

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t cocoa-eh # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo55.6 -t cocoa-eh # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
